cbf0b58749cc128a679bdf7cf100ce12aa640e90
supriya bharane <145009664+supriyabharane@users.noreply.github.com>
2024-07-13 19:44:13 +0530

    Add files via upload

---
+++ BFS graph.cpp
@@ -0,0 +1,21 @@
+vector<int> ans, vis(V, 0);
+        queue<int> q;
+        q.push(0);
+        vis[0]=1;
+        while(!q.empty())
+        {
+            int front= q.front();
+            q.pop();
+            ans.push_back(front);
+            for(auto &it : adj[front])
+            {
+                if(!vis[it])
+                {
+                    q.push(it);
+                   
+                    vis[it]=1;
+                }
+                 
+            }
+        }
+        return ans;
