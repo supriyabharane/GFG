eb122d912e4bdff0fb04edf89d8ab10c4c6af6a6
supriya bharane <145009664+supriyabharane@users.noreply.github.com>
2024-07-14 21:48:26 +0530

    Add files via upload

---
+++ segregates 0 & 1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    void segregate0and1(vector<int> &arr) {
+        int one = 0;
+        for(int i=0;i< arr.size();i++){
+            if(arr[i] == 1){
+                arr[i] = 0;
+                one++;
+            }
+        }
+        int j= arr.size() - 1; 
+        while(one--){
+            arr[j--] = 1;
+        }
+    }
+};
